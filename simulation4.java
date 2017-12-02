eb0d5501e1ae24c4a03da5a91359749c7181f037
tallspider <annie.gao2@student.tdsb.on.ca>
2017-12-02 17:37:39 -0500

    Add files via upload

---
+++ simulation4.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+
+public class simulation4 {
+	public static void main(String[] args)throws IOException, InterruptedException{
+		//Subway line1 = new Subway("line1.txt"); //for(int i = 0; i < line1.numStops; i++) System.out.println(line1.names[i]);
+		//Subway line2 = new Subway("line2.txt"); //for(int i = 0; i < line1.numStops; i++) System.out.println(line1.names[i]);
+		//Subway line3 = new Subway("line3.txt"); //for(int i = 0; i < line1.numStops; i++) System.out.println(line1.names[i]);
+		Subway line4 = new Subway("line4.txt"); //for(int i = 0; i < line1.numStops; i++) System.out.println(line1.names[i]);
+		
+		//for(int i = 0; i < 32; i++)System.out.println(TTCGreen.names[i]);
+		/*while(true){
+			line1.sim(0, 31);
+			line1.sim(31, 0);
+		}
+		*/
+		int[] result = line4.sim(0, 4);
+		System.out.println(result[0]);
+		
+	}
+}
